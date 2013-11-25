827f569deef2ef829aecaaf2d4cad0914bc79275
PatrickKiebitz <patrick_kiebitz@gmx.ch>
2013-11-25 15:46:22 +0100

    SkillsRepository added

---
+++ src/main/java/ch/itraum/recruiter/repository/SkillsRepository.java
@@ -0,0 +1,10 @@
+package ch.itraum.recruiter.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ch.itraum.recruiter.model.Candidate;
+import ch.itraum.recruiter.model.Skills;
+
+public interface SkillsRepository extends CrudRepository<Skills, Integer> {
+
+}
