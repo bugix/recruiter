a3b5da1a4eace6c02c4521387541258bc414c719
PatrickKiebitz <patrick_kiebitz@gmx.ch>
2013-12-11 16:44:20 +0100

    Change: Just one Translation File per Language

--- src/main/java/ch/itraum/recruiter/tools/recruiterHelper.java
+++
@@ -1,8 +0,0 @@
-package ch.itraum.recruiter.tools;
-
-public class recruiterHelper {
-	public static final String FILE_NAME_MOTIVATIONSSCHREIBEN = "Motivationsschreiben_Kiebitz_1977-BG_1976-AK_1974-RM_1973-PG.txt";
-	public static final String LANGUAGE_GERMAN = "de";
-	public static final String LANGUAGE_ENGLISH = "en";
-	public static final String LANGUAGE_DEFAULT = LANGUAGE_GERMAN;
-}
